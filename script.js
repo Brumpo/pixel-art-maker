b96c25461efecd2275b4ba04fac2df3fa66343c8
Brent Schroder <schroder.brent@gmail.com>
2017-10-17 08:30:08 -0600

    Event listeners still not working, added completed plalette

--- script.js
+++ script.js
@@ -1,13 +1,66 @@
 document.addEventListener('DOMContentLoaded',function(){
-    var dady= document.getElementsByClassName('border')[0];
+  var daddy= document.getElementsByClassName('border')[0];
+  var palete = document.querySelector('.palete');
   for(var i=0;i<784;i++){
     var cubes = document.createElement('div')
-    cubes.class=`tile ${i}`;
-    cubes.style.border='2px solid #8c520b';
+    cubes.className=`tile`;
+    cubes.style.border='1px solid black';
     cubes.style.float='left';
-    cubes.style.width='11px';
-    cubes.style.height='11px';
-    dady.appendChild(cubes);
+    cubes.style.width='13px';
+    cubes.style.height='13px';
+    daddy.appendChild(cubes);
   }
-  
+  getPalete();
+  var colorselected='white';
+  palete. addEventListener('click',selectcolor());
+  console.log(colorselected);
+  // daddy.addEventListener('click')
+
 })
+var getPalete = function() {
+   var palete = document.querySelector('.palete');
+
+   var colors = [
+     '#f44242',
+     '#f45941',
+     '#f48241',
+     '#f4b541',
+     '#f4ee41',
+     '#bef441',
+     '#97f441',
+     '#41f44f',
+     '#41f4a0',
+     '#41f4ee',
+     '#4182f4',
+     '#4155f4',
+     '#7641f4',
+     '#d041f4',
+     '#f441be',
+     '#f44179',
+     '#f44141',
+     '#774c02',
+     '#3c7702',
+     '#022977',
+     '#770202',
+     '#000000',
+     '#756d6d',
+     '#efbaba',
+     '#ffffff',
+   ];
+
+   for (var k of colors) {
+     var color = document.createElement('div');
+     color.className=`${k}`
+     color.style.backgroundColor= k;
+     color.style.border = '1px solid black';
+     color.style.float='left';
+     color.style.width='25px';
+     color.style.height='25px';
+     color.style.margin='5px';
+     palete.appendChild(color);
+   }
+ }
+ var selectcolor=function(){
+   var colorselected= event.target;
+     console.log(colorselected);
+ }
